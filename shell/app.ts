673149f4ac7bdf1abfa53adb70b8757ce9e78a53
Jasmine <jasmine758@rediffmail.com>
2022-12-23 22:13:51 +0530

    initial commit

---
+++ shell/app.ts
@@ -0,0 +1,37 @@
+import {fromEvent} from 'rxjs';
+import * as sharedLib  from 'shared-lib';
+
+const container = document.getElementById('container');
+const flightsLink = document.getElementById('flights');
+const homeLink = document.getElementById('home');
+
+function removeFirstChild() {
+    if (container.firstChild) {
+        container.firstChild.remove();
+    }
+}
+
+function displayWelcomeMessage() {
+    removeFirstChild();
+    container.innerHTML = `<h1>Welcome!</h1>`;
+}
+
+(async function() { 
+   
+   
+    sharedLib.setData('Hello from the Shell!');
+
+    displayWelcomeMessage();
+
+    fromEvent(flightsLink, 'click').subscribe(async _ => {
+        const module = await import('mfe1/component');
+        const elm = document.createElement(module.elementName);
+        removeFirstChild();       
+        container.appendChild(elm);
+    });
+
+    fromEvent(homeLink, 'click').subscribe(_ => {
+        displayWelcomeMessage();
+    })
+
+})();
